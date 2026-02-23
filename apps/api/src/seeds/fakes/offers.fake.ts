c0620c856ba97d9db870c1e0d20995c3be4f28a1
Tigana <m.aziz.hlel@gmail.com>
2026-02-23 10:03:21 +0100

    added seed thmbnail for offers

--- apps/api/src/seeds/fakes/offers.fake.ts
+++ apps/api/src/seeds/fakes/offers.fake.ts
@@ -1,43 +1,69 @@
 import { prisma } from '@/bootstrap/db.init';
-import { OfferStatus } from '@/generated/prisma/enums';
+import { MediaStatus, OfferStatus } from '@/generated/prisma/enums';
 import { faker } from '@faker-js/faker';
 
 faker.seed(1); // Ensure consistent fake data across runs
 
-const fakeOffersTitle = [
-  'Special Discount on next 5 beers',
-  'Free pizza',
-  '20% off on your next order',
-  'Special package deal: 3 beers + burger',
-  'Free pizza and beer combo',
-  'Beer bucket',
+const fakeOffersData: { title: string; imgUrl: string; baseName: string }[] = [
+  {
+    title: 'Special Discount on next 5 beers',
+    imgUrl: 'offers/5_beers_bucket_offer.webp',
+    baseName: '5_beers_bucket_offer.webp',
+  },
+  { title: 'Free pizza', imgUrl: 'offers/free_pizza_offer.jpg', baseName: 'free_pizza_offer.jpg' },
+  { title: '20% off on your next order', imgUrl: 'offers/20_pourcent_off.jpg', baseName: '20_pourcent_off.jpg' },
+  {
+    title: 'Special package deal: 3 beers + burger',
+    imgUrl: 'offers/3burgers_1beer.jpg',
+    baseName: '3burgers_1beer.jpg',
+  },
+  { title: 'Free pizza and beer combo', imgUrl: 'offers/pizza_and_beer.jpg', baseName: 'pizza_and_beer.jpg' },
+  { title: 'Beer bucket', imgUrl: 'offers/bucket_beer.jpg', baseName: 'bucket_beer.jpg' },
 ];
 
-const createFakeOffer = (_: unknown, index: number) => {
-  if (index >= fakeOffersTitle.length) {
-    throw new Error(`Index ${index} is out of bounds for fakeOffersTitle array.`);
-  }
-
+const createFakeOffer = (
+  _: unknown,
+  { title, imgUrl, baseName }: { title: string; imgUrl: string; baseName: string },
+) => {
   const fakeOffer = {
-    title: fakeOffersTitle[index],
+    title: title,
     description: faker.lorem.paragraph(),
     code: faker.number.int({ min: 10, max: 1000 }).toString(),
     points: faker.number.int({ min: 10, max: 1000 }),
     status: faker.helpers.arrayElement(Object.values(OfferStatus)),
+    thumbnail: {
+      key: imgUrl,
+      baseName: baseName,
+      fileType: 'jpg',
+      mimeType: 'image/jpeg',
+      fileSize: 1024 * 500,
+      status: MediaStatus.CONFIRMED,
+      createdAt: faker.date.past(),
+      updatedAt: faker.date.recent(),
+    },
     createdAt: faker.date.past(),
     updatedAt: faker.date.recent(),
   };
 
   return fakeOffer;
-  
 };
 
+const generateFakeOffer = () => {
+  return faker.image.url();
+};
+
+const fakeOffers = [];
 const seedOffers = async () => {
-  const fakeOffers = faker.helpers.multiple(createFakeOffer, { count: fakeOffersTitle.length });
+  const fakeOffers = fakeOffersData.map((offer) => createFakeOffer(null, offer));
   for (const offer of fakeOffers) {
     await prisma.offer.upsert({
       where: { code: offer.code },
-      create: offer,
+      create: {
+        ...offer,
+        thumbnail: {
+          create: offer.thumbnail,
+        },
+      },
       update: {}, //offer,
     });
   }
