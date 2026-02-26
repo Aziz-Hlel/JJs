d89c17a46dfb03912582b1948f5b29c67be35fe6
Tigana <m.aziz.hlel@gmail.com>
2026-02-26 13:40:20 +0100

    fix bug amoutn point from number to int

--- packages/contracts/src/schemas/points/EarnPointsRequest.ts
+++ packages/contracts/src/schemas/points/EarnPointsRequest.ts
@@ -2,8 +2,11 @@ import z from 'zod';
 
 export const earnPointsRequestSchema = z.object({
   userId: z.uuid().min(1, 'User id is required'),
-  dollarAmount: z.number().min(1, 'Amount is required').max(10000, 'Amount is too large'),
-  
+  dollarAmount: z
+    .number()
+    .min(1, 'Amount is required')
+    .max(10000, 'Amount is too large')
+    .transform((val) => Math.trunc(val)),
 });
 
 export type EarnPointsRequest = z.infer<typeof earnPointsRequestSchema>;
--- packages/contracts/src/schemas/points/EarnQuoteRequest.ts
+++ packages/contracts/src/schemas/points/EarnQuoteRequest.ts
@@ -2,7 +2,11 @@ import z from 'zod';
 
 export const earnQuoteRequest = z.object({
   userRefrenceCode: z.string().min(1, 'User refrence code is required'),
-  amount: z.number().min(1, 'Amount must be at least 1').max(10000, 'Amount is too large'),
+  amount: z
+    .number()
+    .min(1, 'Amount must be at least 1')
+    .max(10000, 'Amount is too large')
+    .transform((val) => Math.trunc(val)),
 });
 
 export type EarnQuoteRequest = z.infer<typeof earnQuoteRequest>;
