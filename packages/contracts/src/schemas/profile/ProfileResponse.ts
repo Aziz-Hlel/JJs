4e261342e899bb785956af12b881cd406020b19a
Tigana <m.aziz.hlel@gmail.com>
2026-03-04 12:53:28 +0100

    makin docker stage

--- packages/contracts/src/schemas/profile/ProfileResponse.ts
+++ packages/contracts/src/schemas/profile/ProfileResponse.ts
@@ -1,6 +1,8 @@
+import { Gender } from "../../types/enums/enums";
+
 export type ProfileResponse = {
   phoneNumber: string | null;
-  gender: 'M' | 'F' | null;
+  gender: Gender | null;
   address: string | null;
   avatar: string | null;
   createdAt: string;
