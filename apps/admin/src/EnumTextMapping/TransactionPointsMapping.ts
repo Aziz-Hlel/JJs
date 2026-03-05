2a687d759e86751cad89162b07220832bf7dd892
Mohamed Aziz Ben Hlel <tigana137@gmail.com>
2026-03-05 19:25:27 +0100

    deletin admin

--- apps/admin/src/EnumTextMapping/TransactionPointsMapping.ts
+++
@@ -1,9 +0,0 @@
-import type { PointsTransactionType } from '@contracts/types/enums/enums';
-
-const TransactionPointsTextMapping: Record<PointsTransactionType, string> = {
-  EARN: 'Earned',
-  REDEEM: 'Redeem',
-  ADJUSTMENT: 'Adjustment',
-};
-
-export default TransactionPointsTextMapping;
--- apps/admin/src/EnumTextMapping/offerStatusTextMapping.ts
+++
@@ -1,9 +0,0 @@
-import type { OfferStatus } from "@contracts/types/enums/enums";
-
-
-const offerStatusTextMapping: Record<OfferStatus, string> = {
-  ACTIVE: 'Active',
-  INACTIVE: 'Inactive',
-};
-
-export default offerStatusTextMapping;
